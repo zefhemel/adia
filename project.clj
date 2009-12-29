4530db6540daa533f8ac9c25a3a625d1be2e305b
Zef Hemel <zef@zef.me>
2009-12-29 09:41:44 +0100

    Leiningen

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject adia "0.1-SNAPSHOT"
             :description "A Clojure web framework"
             :url "http://github.com/zefhemel/adia"
-            :dependencies [[org.clojure/clojure "1.0.0"]
+            :repositories {"scala-tools" "http://scala-tools.org/repo-releases"}
+            :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
                            [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                           [org.clojars.ato/compojure "0.3.1-master"]])
+                           [org.clojars.liebke/compojure "0.3.1-master"]
+                           [org.markdownj/markdownj "0.3.0-1.0.2b4"]
+                           [congomongo "0.1-zefhemel"]])
